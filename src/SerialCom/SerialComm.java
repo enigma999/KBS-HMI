df91f0ef435e6678ddf12f1c033fe17f314cceed
simon <s.hutten@student.utwente.nl>
2023-05-22 15:20:43 +0200

    Serialcomm naar package

--- src/SerialCom/SerialComm.java
+++ src/SerialCom/SerialComm.java
@@ -1,6 +1,7 @@
+package SerialCom;
+
 import com.fazecast.jSerialComm.*;
 
-import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
