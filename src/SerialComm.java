00e61d7f4620b8180f5372da55bc06df1c12a419
simon <s.hutten@student.utwente.nl>
2023-05-22 15:11:43 +0200

    SerialComm read + Write + private method

--- src/SerialComm.java
+++ src/SerialComm.java
@@ -1,20 +1,22 @@
 import com.fazecast.jSerialComm.*;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Scanner;
 
 public class SerialComm implements Runnable{
-    private int baudRate = 250000;
+    private int baudRate = 9600;
     private OutputStream outputStream;
     private Scanner scanner = new Scanner(System.in);
     private String command;
     private boolean isSendingData = false;
     private InputStream inputStream;
+    private SerialPort serialPort;
 
     public SerialComm(String comPort) {
-        SerialPort serialPort = SerialPort.getCommPort(comPort);
+        serialPort = SerialPort.getCommPort(comPort);
         serialPort.setBaudRate(baudRate);
 
         if (serialPort.openPort()) {
@@ -28,68 +30,72 @@ public class SerialComm implements Runnable{
     }
 
     public void stuurCoords(int x, int y) {
+        isSendingData = true;
         command = "c " + "(" + x + ", " + y + ")";
-        byte[] bytes = command.getBytes();
-        scanner.nextLine();
-        try {
-            outputStream.write(bytes);
-            outputStream.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        stuurCommand(command);
 
     }
 
     public void noodstop() {
+        isSendingData = true;
         command = "n";
-        byte[] bytes = command.getBytes();
-        scanner.nextLine();
-
-        try {
-            outputStream.write(bytes);
-            outputStream.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        stuurCommand(command);
     }
 
     public void besturing(boolean automatisch) {
+        isSendingData = true;
         if (automatisch) {
             command = "a";
         } else {
             command = "h";
         }
+        stuurCommand(command);
+
+
+    }
+
+    private void stuurCommand(String command) {
         byte[] bytes = command.getBytes();
-        scanner.nextLine();
-
-        try {
-            outputStream.write(bytes);
-            outputStream.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        serialPort.writeBytes(bytes, bytes.length);
+        isSendingData = false;
     }
     public static void main(String[] args) {
-        SerialComm sc = new SerialComm("COM5");
+        Scanner scanner1 = new Scanner(System.in);
+        SerialComm sc = new SerialComm("COM6");
+        scanner1.nextLine();
+        Thread thread = new Thread(sc);
+        thread.start();
         sc.stuurCoords(3,4);
+        while (true) {
+            scanner1.nextLine();
+            sc.noodstop();
+            scanner1.nextLine();
+            sc.besturing(true);
+
+        }
     }
+
+
 
     public void run() {
         while (true) {
-            if (!isSendingData) {
-                // Read data from the serial port
-                byte[] buffer = new byte[1024];
-                int bytesRead = 0;
-                try {
-                    bytesRead = inputStream.read(buffer);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+            try {
+                if (!isSendingData && inputStream.available() > 0) {
+                    // Read data from the serial port
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = 0;
+                    try {
+                        bytesRead = inputStream.read(buffer);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
 
-                // Process the received data
-                String receivedData = new String(buffer, 0, bytesRead);
-                System.out.println("Received data: " + receivedData);
+                    // Process the received data
+                    String receivedData = new String(buffer, 0, bytesRead);
+                    System.out.println("Received data: " + receivedData);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
 
             // Sleep for a short duration before checking again
