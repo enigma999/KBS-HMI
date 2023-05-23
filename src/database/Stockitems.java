d62fc7a2f51f56f7ed21b2659c0c52258d55af79
marittschool <93706143+marittschool@users.noreply.github.com>
2023-05-23 13:11:54 +0200

    getGewicht

--- src/database/Stockitems.java
+++ src/database/Stockitems.java
@@ -5,14 +5,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Random;
 
-// getCoordinaten() print de coordinaten van een bepaald StockItemID, de input is een int of int[]. De output is een array met coordinaten [x, y] of meerdere arrays met coordinaten [[x, y], [x, y]].
+// getCoordinaten() returnt de coordinaten van een bepaald StockItemID, de input is een int of int[]. De output is een array met coordinaten [x, y] of meerdere arrays met coordinaten [[x, y], [x, y]].
+// getGewicht() returnt het gewicht van een bepaald StockItemID, de input is een int of int[]. De output is een int of een int[] met alle gewichten die bij de StockItemIDs horen.
 // randomXY() zorgt voor het vullen van de coordinaten met random inten (0-4) in de database. Geen input of output.
 
 public class Stockitems extends Connectie {
     public ArrayList<java.lang.String> getCoordinaten(int stockitemid) throws SQLException {
         if (!this.isConnected())
             this.connect();
-        ArrayList<ArrayList<java.lang.String>> result = queryResult("select x, y from stockitemholdings where StockItemID = " + stockitemid);
+        ArrayList<ArrayList<java.lang.String>> result = queryResult("select x, y from stockitemholding where StockItemID = " + stockitemid);
         return (result.get(0));
     }
 
@@ -29,20 +30,42 @@ public class Stockitems extends Connectie {
         return results;
     }
 
+    public int getGewicht(int stockitemid) throws SQLException {
+        if (!this.isConnected())
+            this.connect();
+        ArrayList<ArrayList<String>> result = queryResult("select gewicht from stockitemholding where StockItemID = " + stockitemid);
+        ArrayList<String> resultaat = result.get(0);
+
+        return Integer.parseInt(resultaat.get(0));
+    }
+
+    public int[] getGewicht(int[] stockitemid) throws SQLException {
+        if (!this.isConnected())
+            this.connect();
+
+        int[] results = new int[stockitemid.length];
+
+        for (int i = 0; i < stockitemid.length; i++) {
+            results[i] = getGewicht(stockitemid[i]);
+        }
+
+        return results;
+    }
+
     public void randomXY() throws SQLException {
         if (!this.isConnected())
             this.connect();
 
         Random rand = new Random();
 
-        String selectStockItemIdsQuery = "SELECT stockitemid FROM stockitemholdings";
+        String selectStockItemIdsQuery = "SELECT stockitemid FROM stockitemholding";
         PreparedStatement psStockItemIds = this.connection.prepareStatement(selectStockItemIdsQuery);
         ResultSet stockItemIdsResultSet = psStockItemIds.executeQuery();
 
-        String updateXQuery = "UPDATE stockitemholdings SET x = ? WHERE stockitemid = ?";
+        String updateXQuery = "UPDATE stockitemholding SET x = ? WHERE stockitemid = ?";
         PreparedStatement psX = this.connection.prepareStatement(updateXQuery);
 
-        String updateYQuery = "UPDATE stockitemholdings SET y = ? WHERE stockitemid = ?";
+        String updateYQuery = "UPDATE stockitemholding SET y = ? WHERE stockitemid = ?";
         PreparedStatement psY = this.connection.prepareStatement(updateYQuery);
 
         while (stockItemIdsResultSet.next()) {
