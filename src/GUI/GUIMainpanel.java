31558b94484692cca6ebd5df6793480853367205
marittschool <93706143+marittschool@users.noreply.github.com>
2023-05-31 14:30:47 +0200

    Mainpanel toevoegingen

--- src/GUI/GUIMainpanel.java
+++ src/GUI/GUIMainpanel.java
@@ -1,17 +1,18 @@
 package GUI;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
-import database.Stockitems;
 import java.sql.SQLException;
 import java.util.Arrays;
 import BPP.BPP;
+import database.Stockitems;
 
 public class GUIMainpanel extends JFrame {
     private ArrayList<Integer> clickedSquares = new ArrayList<>();
+    private JTextArea textArea;
 
     public GUIMainpanel() throws SQLException {
         setTitle("Drie-panel GUI");
@@ -46,18 +47,11 @@ public class GUIMainpanel extends JFrame {
         leftPanel.setPreferredSize(new Dimension(400, getHeight()));
         leftPanel.setLayout(new BorderLayout());
 
-        JTextArea textArea = new JTextArea();
+        textArea = new JTextArea();
         textArea.setEditable(false);
         textArea.setMargin(new Insets(10, 10, 10, 10));
-
-        String text = "Order: #29\n" +
-                "Route: 5 --> 9 --> 22\n" +
-                "Tijd: 13,85 seconde(s)\n" +
-                "BPP: Bin 1: [x, x], Bin 2: [x]\n" +
-                "*VOORBEELD*";
-        textArea.setText(text);
-        textArea.setBackground(new Color(220, 220, 220));
         textArea.setFont(new Font("Arial", Font.PLAIN, 20));
+        textArea.setBackground(new Color(220, 220, 220));
 
         leftPanel.add(textArea, BorderLayout.WEST);
 
@@ -70,6 +64,7 @@ public class GUIMainpanel extends JFrame {
 
         ActionListener squareClickListener = new ActionListener() {
             private int clickCount = 0;
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 JButton square = (JButton) e.getSource();
@@ -124,8 +119,8 @@ public class GUIMainpanel extends JFrame {
                     System.out.println(coordinaten.getCoordinaten(clickedSquaresArray)); //coordinaten
                     System.out.println(Arrays.toString(coordinaten.getGewicht(clickedSquaresArray))); //gewicht
                     int[] BinPP = coordinaten.getGewicht(clickedSquaresArray);
-                    System.out.println(binpacking.bestFit(BinPP, clickedSquaresArray)); //bpp
-
+                    ArrayList<ArrayList<Integer>> result = binpacking.bestFit(BinPP, clickedSquaresArray);
+                    displayResult(clickedSquares, result);
                 } catch (SQLException ex) {
                     throw new RuntimeException(ex);
                 }
@@ -151,5 +146,39 @@ public class GUIMainpanel extends JFrame {
     private JButton createButton(String text) {
         JButton button = new JButton(text);
         return button;
+    }
+
+    private void displayResult(ArrayList<Integer> clickedSquares, ArrayList<ArrayList<Integer>> result) {
+        // Convert order numbers to comma-separated string
+        StringBuilder orderBuilder = new StringBuilder();
+        for (int i = 0; i < clickedSquares.size(); i++) {
+            if (i > 0) {
+                orderBuilder.append(", ");
+            }
+            orderBuilder.append(clickedSquares.get(i));
+        }
+        String order = orderBuilder.toString();
+
+        // Convert bin contents to comma-separated strings
+        StringBuilder binBuilder = new StringBuilder();
+        for (int i = 0; i < result.size(); i++) {
+            ArrayList<Integer> bin = result.get(i);
+            StringBuilder binContents = new StringBuilder();
+            for (int j = 0; j < bin.size(); j++) {
+                if (j > 0) {
+                    binContents.append(", ");
+                }
+                binContents.append(bin.get(j));
+            }
+            String binString = "Bin " + (i + 1) + ": " + binContents.toString();
+            binBuilder.append(binString);
+            if (i < result.size() - 1) {
+                binBuilder.append(", ");
+            }
+        }
+        String bins = binBuilder.toString();
+
+        // Display the order and bin information
+        textArea.setText("Order = " + order + "\nBPP = " + bins);
     }
 }
